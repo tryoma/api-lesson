3f30df430a5d9ce52b5c054e85f9e97d68635744
Tryoma <tryoma0326@gmail.com>
2023-01-25 11:38:48 +0900

    third

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,18 @@ async function bootstrap() {
     origin: ['http://localhost:3000'],
   });
   app.use(cookieParser());
-  await app.listen(3005);
+  app.use(
+    csurf({
+      cookie: {
+        httpOnly: true,
+        sameSite: 'none',
+        secure: true,
+      },
+      value: (req: Request) => {
+        return req.header('csrf-token');
+      },
+    }),
+  );
+  await app.listen(process.env.PORT || 3005);
 }
 bootstrap();
